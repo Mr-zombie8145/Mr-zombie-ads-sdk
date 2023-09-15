ace6a03d4ce72280334d13a3087c694078d88448
Mr. zombie <masum.zombie@gmail.com>
2023-09-15 14:47:31 +0530

    add admob ConsentInformation dialog

--- library/src/main/java/com/solodroid/ads/sdk/gdpr/GDPR.java
+++ library/src/main/java/com/solodroid/ads/sdk/gdpr/GDPR.java
@@ -10,6 +10,7 @@ import com.google.android.ump.UserMessagingPlatform;
 
 public class GDPR {
 
+    private static final String TAG ="AdmobAds";
     private ConsentInformation consentInformation;
     ConsentForm consentForm;
     Activity activity;
@@ -43,6 +44,39 @@ public class GDPR {
                 formError -> {
                 }
         );
+    }
+
+
+    public void showGDPRConsent(){
+        ConsentRequestParameters params = new ConsentRequestParameters
+                .Builder()
+                .setTagForUnderAgeOfConsent(false)
+                .build();
+
+        consentInformation = UserMessagingPlatform.getConsentInformation(activity);
+        consentInformation.requestConsentInfoUpdate(
+                activity,
+                params,
+                () -> {
+                    UserMessagingPlatform.loadAndShowConsentFormIfRequired(
+                            activity,
+                            loadAndShowError -> {
+                                if (loadAndShowError != null) {
+                                    // Consent gathering failed.
+                                    Log.w(TAG, String.format("%s: %s",
+                                            loadAndShowError.getErrorCode(),
+                                            loadAndShowError.getMessage()));
+                                }
+                                // Consent has been gathered.
+                            }
+                    );
+                },
+                requestConsentError -> {
+                    // Consent gathering failed.
+                    Log.w(TAG, String.format("%s: %s",
+                            requestConsentError.getErrorCode(),
+                            requestConsentError.getMessage()));
+                });
     }
 
 }
